884e392c6bf602b63850caa9fc7dca11e01f314c
mertkasar <mertkasar93@gmail.com>
2014-06-03 04:35:00 +0300

    Implemented QuickSort solution

--- algorithms/arrays-and-sorting/quick_sort.py
+++ algorithms/arrays-and-sorting/quick_sort.py
@@ -1,3 +1,16 @@
-a = [5, 8, 1, 3, 7, 9, 2]
+N = int(input())
+inp = list(map(int, input().split()))
 
-print(a)
+def sort(arr):
+    if len(arr) == 0 or len(arr) == 1: return arr
+    
+    P = arr[0]
+    
+    left = [i for i in arr if i < P]
+    right = [i for i in arr if i > P]
+     
+    output = sort(left) + [P] + sort(right)
+    print(" ".join(str(i) for i in output))
+    return output
+
+sort(inp)
